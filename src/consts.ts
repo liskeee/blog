3842e371b4871b58b012c17dd899f2e59b979513
Łukasz Lis <ll.lukasz.lis@gmail.com>
2024-10-31 10:51:00 +0000

    feat: astro icons

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,4 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Liskeee Blog - Łukasz Lis';
+export const SITE_TITLE = 'Liske Tech Blog';
 export const SITE_DESCRIPTION =
-  "Whether you're a seasoned developer or just starting out, enhance your coding skills and stay informed with our expert insights.";
+  "Whether you're a seasoned developer or just starting out, enhance your IT knowledge and start your own journey with homelab, cloud, and programming.";
+export const CONTACT_EMAIL = 'll.lukasz.lis@gmail.com';
